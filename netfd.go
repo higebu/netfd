b9205c556c0b69aedc40ee9819e3c6c9bdfe6767
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2022-10-11 10:53:22 +0900

    Fix for latest go version

--- netfd.go
+++ netfd.go
@@ -1,5 +1,3 @@
-// +build !go1.9
-
 package netfd
 
 import (
@@ -8,17 +6,20 @@ import (
 )
 
 // GetFdFromConn get net.Conn's file descriptor.
-func GetFdFromConn(l net.Conn) int {
-	v := reflect.ValueOf(l)
-	netFD := reflect.Indirect(reflect.Indirect(v).FieldByName("fd"))
-	fd := int(netFD.FieldByName("sysfd").Int())
+func GetFdFromConn(c net.Conn) int {
+	v := reflect.Indirect(reflect.ValueOf(c))
+	conn := v.FieldByName("conn")
+	netFD := reflect.Indirect(conn.FieldByName("fd"))
+	pfd := netFD.FieldByName("pfd")
+	fd := int(pfd.FieldByName("Sysfd").Int())
 	return fd
 }
 
 // GetFdFromListener get net.Listener's file descriptor.
 func GetFdFromListener(l net.Listener) int {
-	v := reflect.ValueOf(l)
-	netFD := reflect.Indirect(reflect.Indirect(v).FieldByName("fd"))
-	fd := int(netFD.FieldByName("sysfd").Int())
+	v := reflect.Indirect(reflect.ValueOf(l))
+	netFD := reflect.Indirect(v.FieldByName("fd"))
+	pfd := netFD.FieldByName("pfd")
+	fd := int(pfd.FieldByName("Sysfd").Int())
 	return fd
 }
